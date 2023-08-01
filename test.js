e139f1d9be0a1d32d8cb0e74cb9f196dd5154612
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-08-01 21:36:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,14 @@
-function reverse(number) {
-  let reversedNumber = 0;
-  let isNegative = number < 0;
-  
-  // Convert the number to positive if it is negative
-  if (isNegative) {
-    number *= -1;
+ export function swap (massive) {
+   for (let i = 0; i = massive.length - 1; i++) {
+     let temp1;
+      if (massive.length > 1) {
+         temp1 = massive[0];
+         massive[0] = massive[i];
+         massive[i] = temp1;
+         return massive;
+}
+  else {
+    return massive;
   }
-  
-  // Reverse the digits of the number
-  while (number > 0) {
-    const digit = number % 10;
-    reversedNumber = (reversedNumber * 10) + digit;
-    number = Math.floor(number / 10);
-  }
-  
-  // Convert the number back to negative if necessary
-  if (isNegative) {
-    reversedNumber *= -1;
-  }
-  
-  return reversedNumber;
+   }
 }
